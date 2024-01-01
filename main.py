b60840b537032f844e7add382a33e9c154a58539
Mohana M <145038131+somuchmoh@users.noreply.github.com>
2024-01-01 17:13:57 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['POST'])
+def receive_data():
+    return f"<h1>Name: {request.form['username']}, Password: {request.form['password']} </h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
